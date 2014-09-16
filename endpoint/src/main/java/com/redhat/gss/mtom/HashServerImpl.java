3f4cc5a3f9a5e88679f28dc43988d2ef2d5fb28e
Kyle Lape <kyle.lape@redhat.com>
2014-09-15 22:46:00 -0700

    Making changes to force temp file to be written

--- endpoint/src/main/java/com/redhat/gss/mtom/HashServerImpl.java
+++ endpoint/src/main/java/com/redhat/gss/mtom/HashServerImpl.java
@@ -9,32 +9,58 @@ import org.jboss.logging.Logger;
 import java.math.BigInteger;
 import org.apache.cxf.annotations.EndpointProperty;
 import org.apache.cxf.annotations.EndpointProperties;
+import java.lang.reflect.Method;
+import javax.xml.ws.WebServiceContext;
+import javax.annotation.Resource;
+import javax.xml.ws.handler.MessageContext;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.jaxws.context.WrappedMessageContext;
+import java.util.Collection;
 
 @MTOM
 @WebService(endpointInterface="com.redhat.gss.mtom.HashServer")
-@EndpointProperties({
-  @EndpointProperty(key="attachment-memory-threshold", value="4096")
-})
-public class HashServerImpl implements HashServer
-{
+public class HashServerImpl implements HashServer {
   private static Logger log = Logger.getLogger(HashServer.class);
 
-	public String calcHash(ContentDataType data) throws Exception {
-    DataHandler dh = data.getContentData();
-    InputStream input = dh.getInputStream();
-    MessageDigest digest = MessageDigest.getInstance("MD5");
-    byte[] bb = new byte[1024];
-    int totalLength = 0;
-    int length = 0;
-    while((length = input.read(bb)) > 0) {
-      totalLength += length;
-      digest.update(bb, 0, length);
+  @Resource
+  WebServiceContext ctx;
+
+  public String calcHash(ContentDataType data) throws Exception {
+    try {
+      Message message = ((WrappedMessageContext)ctx.getMessageContext()).getWrappedMessage();
+      Collection c = message.getAttachments();
+      for(Object o : c) {
+        //Don't do anything
+        //Just iterating through the attachments will force CXF to cache them
+        //Which will make it analyze the size vs threshold
+      }
+
+      DataHandler dh = data.getContentData();
+      InputStream input = dh.getInputStream();
+
+      // Method m = input.getClass().getDeclaredMethod("getInputStream");
+      // Object delegate = m.invoke(input);
+      // delegate = m.invoke(delegate);
+      // log.info("Delegate InputStream: " + delegate.getClass().getName());
+
+      MessageDigest digest = MessageDigest.getInstance("MD5");
+      byte[] bb = new byte[2048];
+      int totalLength = 0;
+      int length = 0;
+      while((length = input.read(bb)) > 0) {
+        totalLength += length;
+        digest.update(bb, 0, length);
+      }
+      input.close();
+      log.info("Total attachment length: " + totalLength);
+      String s = toHexString(digest.digest());
+      log.debug("Digest: " + s);
+      return s;
+    } catch(Exception e) {
+      log.error("", e);
     }
-    log.info("Total attachment length: " + totalLength);
-    String s = toHexString(digest.digest());
-    log.debug("Digest: " + s);
-    return s;
-	}
+    return null;
+  }
 
   private String toHexString(byte[] hash) {
     BigInteger bi = new BigInteger(1, hash);
